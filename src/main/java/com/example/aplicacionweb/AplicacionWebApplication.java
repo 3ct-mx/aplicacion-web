d144abc6cd5ef2edfbef483ab23a8fb1b09e5ebc
Jorge Sanchez <jorgesanchez@Jorges-MacBook-Pro.local>
2023-01-08 09:04:28 -0800

    initial commit

---
+++ src/main/java/com/example/aplicacionweb/AplicacionWebApplication.java
@@ -0,0 +1,25 @@
+package com.example.aplicacionweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+
+
+public class AplicacionWebApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionWebApplication.class, args);
+	}
+
+
+
+
+
+}
---
+++ src/main/java/com/example/aplicacionweb/PersonaController.java
@@ -0,0 +1,18 @@
+package com.example.aplicacionweb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v2/personas")
+public class PersonaController {
+
+    @GetMapping
+    public List<Persona> getPersonas(){
+        return
+                List.of(new Persona("Ana",9));
+    }
+}
